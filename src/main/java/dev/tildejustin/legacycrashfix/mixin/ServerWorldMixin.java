2dd0c2470f6d7cf1a1878081c31d76ca228b80b5
tildejustin <spacepepper3.14@gmail.com>
2024-07-24 21:13:50 -0400

    1.6- fix

--- src/main/java/dev/tildejustin/legacycrashfix/mixin/ServerWorldMixin.java
+++ src/main/java/dev/tildejustin/legacycrashfix/mixin/ServerWorldMixin.java
@@ -3,14 +3,18 @@ package dev.tildejustin.legacycrashfix.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.tildejustin.legacycrashfix.NextTickListEntryHashSet;
 import net.minecraft.server.world.ServerWorld;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.HashSet;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin {
-    @ModifyExpressionValue(method = "<init>", at = @At(value = "NEW", target = "java/util/HashSet"))
+    @Dynamic
+    @ModifyExpressionValue(
+            method = {"<init>", /* 1.6- */ "<init>(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/class_101;Ljava/lang/String;ILnet/minecraft/class_1156;Lnet/minecraft/class_839;Lnet/minecraft/class_1555;)V"},
+            at = @At(value = "NEW", target = "java/util/HashSet"), require = 1
+    )
     private HashSet<?> useHashSetPreservingEqualitySemantics(HashSet<?> original) {
         return NextTickListEntryHashSet.newHashSet();
     }
